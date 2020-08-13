5b819d7d0c6a1c1bc0faab885d6c3b3cb8c3a82a
Matheus Monteiro <monteiromatheus@id.uff.br>
2020-08-12 21:21:11 -0300

    Implement findById( )

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,11 +21,11 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Department obj = new Department(1,"Books");
-        Seller seller = new Seller(1, "Bob", "bob@gmail.com",new Date(),3000.00,obj);
+
         
         SellerDao sellerdao = DaoFactory.createSellerDao();
+        
+        Seller seller = sellerdao.findById(3);
         
         System.out.println(seller);
     }
